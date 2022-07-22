34f1a17a407898813ebc119e38bbac9ffc7bd2e8
tobias ferreira <soutobias@yahoo.com.br>
2022-07-22 10:10:43 -0300

    cloud env

--- app/javascript/plugins/get_image.js
+++ app/javascript/plugins/get_image.js
@@ -6,9 +6,20 @@ const getImage = (startDate) => {
   const dataElement = document.getElementById('data');
   
   if (dataElement) {
+    const cloudn = dataElement.dataset.cloudn;
+    const clouds = dataElement.dataset.clouds;
+    const clouda = dataElement.dataset.clouda;
 
-    const cloud = dataElement.dataset.cloudinary;
-    cloudinary.config(cloud);
+    cloudinary.config({ 
+      cloud_name: cloudn, 
+      api_key: clouda, 
+      api_secret: clouds
+    });
+    // cloudinary.config()['cloud_name'] = cloudn
+    // cloudinary.config()['api_key'] = clouda
+    // cloudinary.config()['api_secret'] = clouds
+
+
     return cloudinary.url(`${startDate}.png`)
   }
 };
